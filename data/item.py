fd4f9fc8c3569f5b8b2d07076d59171f49077737
KKcloud4236 <56917703+KKcloud4236@users.noreply.github.com>
2020-03-25 16:38:45 +0900

    アイテム情報を分けた(修正要る)

--- data/item.py
+++ data/item.py
@@ -1,27 +1,27 @@
-from RpgError import LostError, UnknownTypeError
-
-
 class Item():
-    def __init__(name: str,  # アイテム名
-                 prise: int,  # 販売される価格
-                 sold: int,  # 売った時の価格
-                 description: str,  # 説明
-                 types="",  # アイテム属性,
-                 canBuy: bool,  # 買えるかどうか
-                 rename=None  # ユーザー独自の名前
-                 ):
-        self.typeslist = [
-            "recovery",  # 回復系アイテム
-            "change",  # 変更系アイテム
-            "material",  # 素材系アイテム
-            "quest",  # クエスト系アイテム
-        ]
-        if types in self.typeslist:
-            self.itemTypes = types
-        else:
-            raise UnknownTypeError("Doesn't match any item types")
+    def __init__(self, name: str, prise: int, sold: int, description: str, canBuy: bool, rename=None):
         self.name = name
-        self.prose = prise if canBuy else None
+        self.prise = prise if canBuy else None
         self.sold = sold
         self.description = description
         self.rename = rename if rename is not None else name
+
+
+class QuestItem(Item):
+    def __init__(self, name: str, prise: int, sold: int, description: str, canBuy: bool, rename=None):
+        super().__init__(name, prise, sold, description, canBuy, rename)
+
+
+class RecoveryItem(Item):
+    def __init__(self, name: str, prise: int, sold: int, description: str, canBuy: bool, rename=None):
+        super().__init__(name, prise, sold, description, canBuy, rename)
+
+
+class ChangeItem(Item):
+    def __init__(self, name: str, prise: int, sold: int, description: str, canBuy: bool, rename=None):
+        super().__init__(name, prise, sold, description, canBuy, rename)
+
+
+class MaterialItem(Item):
+    def __init__(self, name: str, prise: int, sold: int, description: str, canBuy: bool, rename=None):
+        super().__init__(name, prise, sold, description, canBuy, rename)
