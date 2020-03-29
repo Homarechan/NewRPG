beeca4434b5f1ef0b2a7ff2a6d7ab602c9bf2577
KKcloud4236 <56917703+KKcloud4236@users.noreply.github.com>
2020-03-29 16:25:05 +0900

    キャラクターに防具つけてみた

--- lib/user.py
+++ lib/user.py
@@ -1,8 +1,20 @@
 from .job import Apprentice
+from .armor import (
+    Poor_Head,
+    Poor_Body,
+    Poor_Leggings,
+    Poor_Boots
+)
 
 
-class Adventurer(Apprentice):
+class Adventurer():
     def __init__(self, name):
         self.userName = name
         self.item = {}
         self.job = Apprentice()
+        self.armor = {
+            "head": Poor_Head(),
+            "body": Poor_Body(),
+            "leggings": Poor_Leggings(),
+            "boots": Poor_Boots()
+        }
